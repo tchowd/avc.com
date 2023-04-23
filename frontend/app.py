e2904718a5d5b57675a314df3ed416318e1cf2f2
Turja Chowdhury <42366823+turja-c@users.noreply.github.com>
2023-04-23 00:31:33 -0400

    deployment testing - frontend complete

--- frontend/app.py
+++ frontend/app.py
@@ -1,7 +1,8 @@
 # app.py
 
-from flask import Flask, render_template, request, send_file
-import scraper
+from flask import Flask, render_template, request, send_file, Response
+import scraper 
+import os
 
 app = Flask(__name__)
 
@@ -10,8 +11,14 @@ def index():
     if request.method == 'POST':
         base_url = 'https://avc.com'
         max_workers = 10
-        csv_file = scraper.run_scraper(base_url, max_workers)
-        return send_file(csv_file, mimetype='text/csv', attachment_filename='all_posts.csv', as_attachment=True)
+        csv_file_path = scraper.run_scraper(base_url, max_workers)
+        with open(csv_file_path, "r") as f:
+            csv_data = f.read()
+
+        response = Response(csv_data, content_type='text/csv')
+        response.headers['Content-Disposition'] = 'attachment; filename=all_posts.csv'
+        return response
+
     return render_template('index.html')
 
 if __name__ == '__main__':
